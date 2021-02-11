aeb5965495affe6e15f9c63d894194eb23e77eeb
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-02-10 23:06:11 -0300

    Updates scripts to allow testing on Chapel in CI

--- scripts/start-chapel-fork.sh
+++ scripts/start-chapel-fork.sh
@@ -7,7 +7,7 @@ source .env
 npx ganache-cli \
 	-q \
 	-h 0.0.0.0 \
-	-i 56 \
+	-i 97 \
 	-l $GAS_LIMIT \
-	-f https://bsc-dataseed.binance.org/ \
+	-f https://data-seed-prebsc-1-s1.binance.org:8545/ \
 	--account $PRIVATE_KEY,100000000000000000000
