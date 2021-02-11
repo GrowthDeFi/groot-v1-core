3d40a2a55e988c7a5e55ba791dd4208380128367
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-02-11 12:56:18 -0300

    Makes airdrop address registration in chunks

--- migrations/02_deploy_contracts.js
+++ migrations/02_deploy_contracts.js
@@ -1,6 +1,14 @@
 const Deployer = artifacts.require('Deployer');
 const LibDeployer = artifacts.require('LibDeployer');
 
+function chunks(array, size = 100) {
+  const result = [];
+  for (let i = 0, j = size; i < array.length; i = j, j += size) {
+      result.push(array.slice(i, j));
+  }
+  return result;
+}
+
 module.exports = async (deployer, network, [account]) => {
   await deployer.deploy(LibDeployer);
   deployer.link(LibDeployer, Deployer);
@@ -8,11 +16,19 @@ module.exports = async (deployer, network, [account]) => {
 
   const listPMINE = require('./listPMINE.json');
   console.log('Adding ' + listPMINE.length + ' PMINE wallets...');
-  await contract.registerReceiversPMINE(listPMINE.map(([address,]) => address), listPMINE.map(([,units]) => BigInt(units) * 10n ** 12n));
+  for (const list of chunks(listPMINE)) {
+    const addresses = list.map(([address,]) => address);
+    const amounts = list.map(([,units]) => BigInt(units) * 10n ** 12n);
+    await contract.registerReceiversPMINE(addresses, amounts);
+  }
 
   const listSAFE = require('./listSAFE.json');
   console.log('Adding ' + listSAFE.length + ' SAFE wallets...');
-  await contract.registerReceiversSAFE(listSAFE.map(([address,]) => address), listSAFE.map(([,cents]) => BigInt(cents) * 10n ** 16n));
+  for (const list of chunks(listSAFE)) {
+    const addresses = list.map(([address,]) => address);
+    const amounts = list.map(([,cents]) => BigInt(cents) * 10n ** 16n);
+    await contract.registerReceiversSAFE(addresses, amounts);
+  }
 
   if (['development'].includes(network)) {
     await contract.deploy();
