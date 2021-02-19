8de3b06b22943d53ce0e2543f333a36b87364493
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-02-19 16:53:30 -0300

    Patches migrations BNB allocation to match Deployer's parameter

--- migrations/02_deploy_contracts.js
+++ migrations/02_deploy_contracts.js
@@ -41,7 +41,7 @@ module.exports = async (deployer, network, [account]) => {
 
   if (['development'].includes(network)) {
     console.log('Performing the deploy...');
-    await contract.deploy({ value: Number(700n * 10n ** 18n) });
+    await contract.deploy({ value: Number(30n * 10n ** 18n) });
 
     console.log('Performing the airdrop...');
     await contract.airdrop();
