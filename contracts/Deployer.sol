f679755067cda921f39da922b4841d6e0041c8ae
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-02-20 12:05:34 -0300

    Updates address and asset distribution upon deployment

--- contracts/Deployer.sol
+++ contracts/Deployer.sol
@@ -21,7 +21,14 @@ import { $ } from "./network/$.sol";
 
 contract Deployer is Ownable
 {
-	address constant GROOT_TREASURY = 0x2165fa4a32B9c228cD55713f77d2e977297D03e8;
+	address constant GROOT_TREASURY1 = 0x2165fa4a32B9c228cD55713f77d2e977297D03e8; // G
+	address constant GROOT_TREASURY2 = 0x6248f5783A1F908F7fbB651bb3Ca27BF7c9f5022; // M
+	address constant GROOT_TREASURY3 = 0xBf70B751BB1FC725bFbC4e68C4Ec4825708766c5; // S
+
+	address constant GROOT_INITIAL_STAKE_HOLDER = 0x5c327D395D0617f5b6ad6E8Da5dCBb35A6Be5b11;
+	address constant GROOT_DEFAULT_FEE_COLLECTOR = 0xB0632a01ee778E09625BcE2a257e221b49E79696;
+
+	address constant GROOT_CONTRACTS_OWNER = 0xBf70B751BB1FC725bFbC4e68C4Ec4825708766c5;
 
 	uint256 constant GROOT_TOTAL_SUPPLY = 20000e18; // 20,000
 	uint256 constant GROOT_TREASURY_ALLOCATION = 10420e18; // 10,420
@@ -129,21 +136,30 @@ contract Deployer is Ownable
 		// create and configure compounding strategy contract for gROOT/BNB
 		stkgROOT_BNB = LibDeployer4.publishSTKGROOTBNB(masterChef, 1, gROOT);
 		GRewardCompoundingStrategyToken(stkgROOT_BNB).setExchange(exchange);
-		GRewardCompoundingStrategyToken(stkgROOT_BNB).setTreasury(GROOT_TREASURY);
+		GRewardCompoundingStrategyToken(stkgROOT_BNB).setTreasury(GROOT_DEFAULT_FEE_COLLECTOR);
 
 		// stake gROOT/BNB LP shares into strategy contract
 		if (STAKE_LP_SHARES) {
 			Transfers._approveFunds(gROOT_WBNB, stkgROOT_BNB, _lpshares);
 			GRewardCompoundingStrategyToken(stkgROOT_BNB).deposit(_lpshares);
-			Transfers._pushFunds(stkgROOT_BNB, GROOT_TREASURY, _lpshares);
+			Transfers._pushFunds(stkgROOT_BNB, GROOT_INITIAL_STAKE_HOLDER, _lpshares);
 		} else {
-			Transfers._pushFunds(gROOT_WBNB, GROOT_TREASURY, _lpshares);
+			Transfers._pushFunds(gROOT_WBNB, GROOT_INITIAL_STAKE_HOLDER, _lpshares);
 		}
 
-		// transfer treasury and farming funds to the treasury
-		Transfers._pushFunds(gROOT, GROOT_TREASURY, GROOT_TREASURY_ALLOCATION);
-		Transfers._pushFunds(gROOT, GROOT_TREASURY, GROOT_FARMING_ALLOCATION);
+		// transfer treasury funds to the treasury
+		Transfers._pushFunds(gROOT, GROOT_TREASURY1, GROOT_TREASURY_ALLOCATION / 3);
+		Transfers._pushFunds(gROOT, GROOT_TREASURY2, GROOT_TREASURY_ALLOCATION / 3);
+		Transfers._pushFunds(gROOT, GROOT_TREASURY3, GROOT_TREASURY_ALLOCATION - 2 * (GROOT_TREASURY_ALLOCATION / 3));
 
+		// transfer farming funds to the treasury
+		Transfers._pushFunds(gROOT, GROOT_TREASURY1, GROOT_FARMING_ALLOCATION / 3);
+		Transfers._pushFunds(gROOT, GROOT_TREASURY2, GROOT_FARMING_ALLOCATION / 3);
+		Transfers._pushFunds(gROOT, GROOT_TREASURY3, GROOT_FARMING_ALLOCATION - 2 * (GROOT_FARMING_ALLOCATION / 3));
+
+		require(Transfers._getBalance(WBNB) == 0, "WBNB left over");
+		require(Transfers._getBalance(gROOT_WBNB) == 0, "gROOT_WBNB left over");
+		require(Transfers._getBalance(stkgROOT_BNB) == 0, "stkgROOT_BNB left over");
 		require(Transfers._getBalance(gROOT) == GROOT_AIRDROP_ALLOCATION, "gROOT amount mismatch");
 		require(Transfers._getBalance(SAFE) == SAFE_AIRDROP_ALLOCATION, "SAFE amount mismatch");
 
@@ -157,10 +173,10 @@ contract Deployer is Ownable
 		Ownable(gROOT).transferOwnership(masterChef);
 		Ownable(stkgROOT).transferOwnership(masterChef);
 
-		Ownable(registry).transferOwnership(GROOT_TREASURY);
-		Ownable(SAFE).transferOwnership(GROOT_TREASURY);
-		Ownable(masterChef).transferOwnership(GROOT_TREASURY);
-		Ownable(stkgROOT_BNB).transferOwnership(GROOT_TREASURY);
+		Ownable(registry).transferOwnership(GROOT_CONTRACTS_OWNER);
+		Ownable(SAFE).transferOwnership(GROOT_CONTRACTS_OWNER);
+		Ownable(masterChef).transferOwnership(GROOT_CONTRACTS_OWNER);
+		Ownable(stkgROOT_BNB).transferOwnership(GROOT_CONTRACTS_OWNER);
 
 		// wrap up the deployment
 		deployed = true;
