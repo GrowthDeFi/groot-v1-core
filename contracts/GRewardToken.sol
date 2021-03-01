b4d819d69fbf4f14e592cd3a9d609d51404b1aeb
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-03-01 13:11:00 -0300

    More adjustments towards deploying a new MasterChef and compounding strategy

--- contracts/GRewardToken.sol
+++ contracts/GRewardToken.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.6.0;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import { Math } from "./modules/Math.sol";
-
-contract GRewardToken is Ownable, ERC20
+contract GRewardToken is ERC20
 {
 	constructor (string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply)
 		ERC20(_name, _symbol) public
@@ -14,19 +11,5 @@ contract GRewardToken is Ownable, ERC20
 		address _sender = msg.sender;
 		_setupDecimals(_decimals);
 		_mint(_sender, _initialSupply);
-	}
-
-	function allocateReward(uint256 _amount) external
-	{
-		address _from = msg.sender;
-		address _to = address(this);
-		_transfer(_from, _to, _amount);
-	}
-
-	function mint(address _to, uint256 _amount) external onlyOwner
-	{
-		address _from = address(this);
-		uint256 _balance = balanceOf(_from);
-		_transfer(_from, _to, Math._min(_balance, _amount));
 	}
 }
